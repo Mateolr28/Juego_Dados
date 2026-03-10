b8716679914bf58899b410f9726ac572575a78c4
MATEO LARGO <mateolrdev@outlook.com>
2026-03-10 14:06:39 -0500

    Actualizacion

--- src/components/KamasutraGame.tsx
+++ src/components/KamasutraGame.tsx
@@ -11,34 +11,34 @@ interface Position {
 
 const POSITIONS: Position[] = [
   {
-    name: "El Misionero",
-    description: "Una posición clásica que permite un contacto visual íntimo y cercanía emocional.",
-    image: "https://picsum.photos/seed/kama1/600/400"
+    name: "Lanzadera",
+    description: "El hombre se acuesta de espaldas, con las piernas ligeramente dobladas en las rodillas y tirando de los lados. La mujer está encima de la pareja masculina con la cara hacia él de manera que una pierna está entre las piernas de ella. Una de las manos de la mujer está recta y se apoya en el suelo, la segunda está doblada en el codo y está sobre el pecho de la pareja masculina. El hombre pone sus manos en las nalgas de la pareja femenina y es capaz de controlar el ritmo.",
+    image: "https://sexpositions.club/es/wp-content/uploads/sites/2/2016/02/1_3_3.png"
   },
   {
-    name: "La Cuchara",
-    description: "Ideal para la relajación y la ternura, permitiendo un contacto corporal total.",
-    image: "https://picsum.photos/seed/kama2/600/400"
+    name: "Sirena",
+    description: "El hombre se acuesta de espaldas con las piernas ligeramente dobladas por las rodillas y las separa ampliamente. La mujer se tumba encima de su pareja, las piernas están apretadas y ligeramente dobladas por las rodillas, con las manos se sujeta las pantorrillas y levanta un poco el cuerpo hacia las piernas, de manera que está sentada en el suelo. El hombre pone su mano en el pecho de su pareja femenina y lo acaricia durante el sexo.",
+    image: "https://sexpositions.club/es/wp-content/uploads/sites/2/2016/02/2_4_4.png"
   },
   {
-    name: "El Puente",
-    description: "Una posición que requiere flexibilidad y ofrece una perspectiva única.",
-    image: "https://picsum.photos/seed/kama3/600/400"
+    name: "Gato",
+    description: "La mujer se tumba de espaldas, sobre los codos levanta la parte superior de su cuerpo, las piernas están rectas, pero separadas. El hombre se acuesta sobre su compañera cara a cara, mantiene la parte superior del cuerpo sobre los brazos extendidos, las piernas están juntas y situadas entre las piernas de la compañera. Parece estar colgado sobre su dama y puede besar su cuello, sus labios en el proceso.",
+    image: "https://sexpositions.club/es/wp-content/uploads/sites/2/2016/02/3_2_3.png"
   },
   {
-    name: "La Flor de Loto",
-    description: "Fomenta la conexión espiritual y el equilibrio entre ambos.",
-    image: "https://picsum.photos/seed/kama4/600/400"
+    name: "Batiente",
+    description: " La mujer se pone de rodillas y se inclina completamente hacia delante, estirando los brazos por encima de la cabeza, dándoles forma de halo. El hombre se sitúa a la pareja femenina de tal manera, que sus nalgas se encuentran sobre los pies de la mujer, las piernas están muy separadas y la mujer se encuentra entre sus piernas. Las manos del hombre se echan hacia atrás, se ponen sobre sus manos y se hace el apoyo principal sobre ellas.",
+    image: "https://sexpositions.club/es/wp-content/uploads/sites/2/2016/02/4_1_3.png"
   },
   {
-    name: "El Columpio",
-    description: "Añade un elemento de juego y dinamismo a la experiencia.",
-    image: "https://picsum.photos/seed/kama5/600/400"
+    name: "Acuario",
+    description: "La mujer se tumba abriendo las piernas, y el hombre se coloca entre ellas de rodillas para levantar a la compañera por la cintura, de modo que sólo la cabeza y el antebrazo puedan ser su apoyo. La mujer lanza sus piernas a los muslos del hombre, doblándolas por las rodillas, apoya sus nalgas con los brazos, ayudando a la pareja masculina a mantener el ritmo del sexo. En esta posición ambos miembros de la pareja tienen las manos ocupadas, pero tienen los ojos para mirarse y a veces eso es más que suficiente.",
+    image: "https://sexpositions.club/es/wp-content/uploads/sites/2/2016/02/5_7.png"
   },
   {
-    name: "La Mariposa",
-    description: "Enfocada en la suavidad y el ritmo pausado.",
-    image: "https://picsum.photos/seed/kama6/600/400"
+    name: "A horcajadas",
+    description: "El hombre se tumba de espaldas y dobla ligeramente una pierna por la rodilla. La pareja femenina se sienta sobre el pene de forma que sus piernas queden en un lado junto a la pierna doblada de su pareja masculina, que la pareja femenina coge con la mano por la rodilla y la otra mano la pone en el pecho del hombre. La pareja masculina pone su brazo en el cuello de la mujer y toma su rodilla por la otra. La mujer se desplaza en esta posición.",
+    image: "https://sexpositions.club/es/wp-content/uploads/sites/2/2016/02/6_1_2.png"
   }
 ];
 
